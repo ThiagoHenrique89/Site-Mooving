0da977c1d4c0585050ed953acda60fc641a45867
Thiago <thiagohenriqueweb@gmail.com>
2025-03-28 04:17:00 -0300

    src

--- src/includes/header.php
+++ src/includes/header.php
@@ -15,25 +15,25 @@
     <meta name="author" content="Mooving">
     
 	<!-- Open Graph para Facebook e LinkedIn -->
-	<meta property="og:image" content="https://www.mooving.com.br/assets/ico/og-facebook.jpg">
+	<meta property="og:image" content="https://www.mooving.com.br/src/assets/ico/og-facebook.jpg">
 	<meta property="og:image:width" content="1200">
 	<meta property="og:image:height" content="630">
 	<meta property="og:image:alt" content="Imagem otimizada para Facebook e LinkedIn">
 
 	<!-- Twitter Card para Twitter/X -->
 	<meta name="twitter:card" content="summary_large_image">
-	<meta name="twitter:image" content="https://www.mooving.com.br/assets/ico/og-twitter.jpg">
+	<meta name="twitter:image" content="https://www.mooving.com.br/src/assets/ico/og-twitter.jpg">
 	<meta name="twitter:image:alt" content="Imagem otimizada para Twitter">
 
 	<!-- Meta para Instagram (Usa Open Graph padrão) -->
-	<meta property="og:image" content="https://www.mooving.com.br/assets/ico/og-instagram.jpg">
+	<meta property="og:image" content="https://www.mooving.com.br/src/assets/ico/og-instagram.jpg">
 	<meta property="og:image:alt" content="Imagem otimizada para Instagram">
     
     <!-- Favicon -->
-    <link rel="shortcut icon" href="/assets/ico/favicon.png">
+    <link rel="shortcut icon" href="/src/src/assets/ico/favicon.png">
 
     <!-- CSS -->
-    <link rel="stylesheet" type="text/css" href="assets/style.css">
+    <link rel="stylesheet" type="text/css" href="/src/assets/style.css">
 
     <!-- Google Fonts: Poppins -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
@@ -42,7 +42,7 @@
 <body>
 <nav class="navbar">
     <div class="displayMenu container">
-		<div class="logo"><a href="/"><img src="/assets/images/logo-verde.png" alt="Logo"></a></div>
+		<div class="logo"><a href="/"><img src="/src/assets/images/logo-verde.png" alt="Logo"></a></div>
         <div class="barra">
             <div class="menu">
                 <a href="/servicos">SERVIÇOS</a>
