a473170ad751ff65e475b4f6356a272dcdc75950
Thiago <thiagohenriqueweb@gmail.com>
2025-08-26 14:08:39 -0300

    att imagem

--- paginas/home.php
+++ paginas/home.php
@@ -2,7 +2,7 @@
     <div class="banner">
       <picture>
         <source srcset="/assets/images/mosaico-header-bg-verde-celular.webp" media="(max-width: 768px)">
-        <img src="/assets/images/mosaico-header-bg-verde.webp" alt="Agência de Marketing Digital em Jundiaí - Mooving" width="1920" height="800">
+        <img src="/assets/images/mosaico-header-bg-verde.webp" alt="Agência de Marketing Digital em Jundiaí - Mooving" width="1920" height="800" fetchpriority="high">
       </picture>
       <div class="container">
         <h1 class="light">Somos uma <strong>agência de marketing digital em Jundiaí</strong> especializada em <strong>publicidade</strong> e <strong>propaganda</strong>, <strong>conectamos marca</strong> e <strong>cliente</strong>.</h1>
