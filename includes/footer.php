e47eacd2ce824bb2769e4faa2fe0de26e00e3cac
Thiago <thiagohenriqueweb@gmail.com>
2025-08-26 16:02:27 -0300

    attt

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+
+
+  <footer class="rodape">
+
+      </div>
+    </div>
+  </footer>
+
+  <!-- Scripts no final com defer -->
+  <script src="/assets/scripts.js" defer></script>
+</body>
+</html>
