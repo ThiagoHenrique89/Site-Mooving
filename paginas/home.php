70b73ce2d58291bad40fac42477e97155d05f622
Thiago <thiagohenriqueweb@gmail.com>
2025-08-26 22:10:21 -0300

    teste

--- paginas/home.php
+++ paginas/home.php
@@ -11,31 +11,6 @@
   </header>
 <main>
     <article>
-		<section class="carrosel-section">
-		<div class="carrossel-palavras">
-			<div class="conteudo-carrossel">
-
-			<!-- Primeiro grupo -->
-			<h2>Criação de sites <span class="separador">*</span></h2>
-			<h2>Branding <span class="separador">*</span></h2>
-			<h2>Identidade visual <span class="separador">*</span></h2>
-			<h2>Gestão de mídias sociais <span class="separador">*</span></h2>
-			<h2>Vídeos animados <span class="separador">*</span></h2>
-			<h2>Anuncie nas redes sociais e Google <span class="separador">*</span></h2>
-			<h2>Otimização de sites (SEO) <span class="separador">*</span></h2>
-
-			<!-- Segundo grupo (repetido para efeito contínuo) -->
-			<h2>Criação de sites <span class="separador">*</span></h2>
-			<h2>Branding <span class="separador">*</span></h2>
-			<h2>Identidade visual <span class="separador">*</span></h2>
-			<h2>Gestão de mídias sociais <span class="separador">*</span></h2>
-			<h2>Vídeos animados <span class="separador">*</span></h2>
-			<h2>Anuncie nas redes sociais e Google <span class="separador">*</span></h2>
-			<h2>Otimização de sites (SEO) <span class="separador">*</span></h2>
-
-			</div>
-		</div>
-		</section>
 
         <section class="primeiraSection">
 			<div class="container">
@@ -49,7 +24,92 @@
 					<p>A Mooving é uma agência full-service que vai além do convencional. Criamos estratégias inovadoras, combinando dados, criatividade e tecnologia para gerar conexões autênticas entre marcas e clientes. Nosso compromisso é entregar soluções que realmente fazem a diferença, fortalecendo negócios e impulsionando resultados que geram crescimento sustentável e relevante no mercado.</p>
 				</div>
 			</div>
-
+			<div class="cards">
+				<div class="card">
+					<img src="/assets/images/web-design-criacao-de-sites.webp" width="313" height="313" alt="Imagem 1">
+					<h3><span class="light">WEB DESIGN<br></span>CRIAÇÃO DE SITES</h3>
+					<p>
+						Desenvolvemos sites claros, objetivos, modernos, atrativos e funcionais, aumentando a sua presença online.
+					</p>
+					<nav class="menuCard">
+						<ul>
+							<li>E-COMMERCE / LOJA VIRTUAL</li>
+							<li>LANDING PAGE</li>
+							<li>SITE INSTITUCIONAL / SITE DINÂMICO</li>
+						</ul>
+					</nav>
+				</div>
+				<div class="card">
+					<img src="/assets/images/branding-identidade-visual.webp" width="313" height="313" alt="Branding Identidade Visual">
+					<h3><span class="light">BRANDING<br></span>IDENTIDADE VISUAL</h3>
+					<p>
+						Uma marca bem construída não apenas atrai, mas também fideliza, tornando-se reconhecida e desejada.
+					</p>
+					<nav class="menuCard">
+						<ul>
+							<li>LOGOTIPO + IDENTIDADE VISUAL</li>
+							<li>REBRANDING</li>
+							<li>NAMING – METODOLOGIA INTERNA</li>
+						</ul>
+					</nav>
+				</div>
+				<div class="card">
+					<img src="/assets/images/gestao-midias-sociais.webp" width="313" height="313" alt="Gestao Midias Sociais">
+					<h3><span class="light">GESTÃO<br></span>MÍDIAS SOCIAIS</h3>
+					<p>
+						Cuidamos das suas redes sociais, aumentando seu engajamento, aproximando e fidelizando clientes.
+					</p>
+					<nav class="menuCard">
+						<ul>
+							<li>CRIATIVOS PARA MÍDIAS SOCIAIS</li>
+							<li>GERENCIAMENTO DE ANÚNCIOS</li>
+							<li>PLANEJAMENTO E ESTRATÉGIA</li>
+						</ul>
+					</nav>
+				</div>
+				<div class="card">
+					<img src="/assets/images/anuncios-trafego-pago.webp" width="313" height="313" alt="Anuncios Trafego Pago">
+					<h3><span class="light">ANÚNCIOS<br></span>TRÁFEGO PAGO</h3>
+					<p>
+						Aumentamos vendas e geramos leads, garantindo que seus conteúdos se conectem com seu público.
+					</p>
+					<nav class="menuCard">
+						<ul>
+							<li>GOOGLE ADS</li>
+							<li>SOCIAL ADS: META, TIKTOK, LINKEDIN</li>
+							<li>CAMPANHAS 360º</li>
+						</ul>
+					</nav>
+				</div>
+				<div class="card">
+					<img src="/assets/images/seo-otimizacao-de-sites.webp" width="313" height="313" alt="Imagem 1">
+					<h3><span class="light">SEO<br></span>OTIMIZAÇÃO DE SITES</h3>
+					<p>
+						Seu site nas primeiras páginas do Google de forma orgânica, sem precisar pagar por clique.
+					</p>
+					<nav class="menuCard">
+						<ul>
+							<li>SEO ON-PAGE (CONTEÚDO)</li>
+							<li>SEO INDEXAÇÃO E PERFORMANCE</li>
+							<li>SEO LOCAL</li>
+						</ul>
+					</nav>
+				</div>
+				<div class="card">
+					<img src="/assets/images/video-audiovisual.webp" width="313" height="313" alt="Imagem 1">
+					<h3><span class="light">VÍDEOS<br></span>AUDIOVISUAL</h3>
+					<p>
+						Criamos vídeos profissionais que dão vida à sua comunicação e fortalecem o conceito e imagem da sua marca.
+					</p>
+					<nav class="menuCard">
+						<ul>
+							<li>VÍDEO PARA MÍDIAS SOCIAIS</li>
+							<li>VÍDEO COMERCIAL (PROPAGANDA)</li>
+							<li>VÍDEO INSTITUCIONAL (APRESENTAÇÃO)</li>
+						</ul>
+					</nav>
+				</div>
+			</div>
 		</section>
 		<section class="marcas">
 			<div class="container">
