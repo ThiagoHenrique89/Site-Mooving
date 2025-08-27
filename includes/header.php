5b88accf3efeec2ef2d5c00e1cf690890aa85905
Thiago <thiagohenriqueweb@gmail.com>
2025-08-26 22:58:24 -0300

    teste

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="format-detection" content="telephone=no">
   <meta name="theme-color" content="#62966c">
   <meta name="robots" content="index, follow">
