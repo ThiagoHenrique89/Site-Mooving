abbc9bd72c94c7679b76f5b2d468bd4459e49fcb
Thiago <thiagohenriqueweb@gmail.com>
2026-08-11 18:16:17 -0300

    att2

--- paginas/home.php
+++ paginas/home.php
@@ -101,7 +101,7 @@
 				<nav class="menuCard">
 					<ul>
 						<li><a href="/anuncios-trafego-pago#google-ads">GOOGLE ADS</a></li>
-						<li><a href="/anuncios-trafego-pago#social-ads-meta-tik-tok-linkedin">SOCIAL ADS: META a</a></li>
+						<li><a href="/anuncios-trafego-pago#social-ads-meta-tik-tok-linkedin">SOCIAL ADS: META</a></li>
 						<li><a href="/anuncios-trafego-pago#campanhas-360">CAMPANHAS 360º</a></li>
 					</ul>
 				</nav>
